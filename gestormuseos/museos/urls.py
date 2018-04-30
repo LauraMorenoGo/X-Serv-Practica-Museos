12cc6e89127521e5a24275198a06865634a6196c
Laura <laura.mo.go@hotmail.com>
2018-04-30 23:00:13 +0200

    Realizada mezcla de codigo.

--- gestormuseos/museos/urls.py
+++ gestormuseos/museos/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-<<<<<<< HEAD
 from museos import urls as museosurls
 from museos import views
-=======
->>>>>>> 80364828b85845e82600acf56158998f81151ec3
+
 
 urlpatterns = [
     # Examples:
@@ -12,8 +10,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'', include(admin.site.urls)),
-<<<<<<< HEAD
     url(r'^museos/id', views.museo)
-=======
->>>>>>> 80364828b85845e82600acf56158998f81151ec3
+
 ]
