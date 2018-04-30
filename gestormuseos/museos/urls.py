e81544c93a2ab5c4b547124021b8228abc1f5b06
Laura <laura.mo.go@hotmail.com>
2018-04-30 22:28:05 +0200

    Nuevas funciones

--- gestormuseos/museos/urls.py
+++ gestormuseos/museos/urls.py
@@ -3,15 +3,11 @@ from django.contrib import admin
 from museos import urls as museosurls
 from museos import views
 
-
-
 urlpatterns = [
     # Examples:
     # url(r'^$', 'gestormuseos.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^museos/', include(museosurls)),
-    url(r'^usuario/', views.usuario),
-    url(r'^$', views.barra)
+    url(r'', include(admin.site.urls)),
+    url(r'^museos/id', views.museo)
 ]
--- gestormuseos/museos/views.py
+++ gestormuseos/museos/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Museo, Configuracion, Comentario
 
-# Create your views here.
+def barra(request):
+    respuesta = "<html>Museos registrados:<br></html>"
+    lista = Museo.objects.all()
+    for pag in lista:
+        respuesta += "<br><li><a href='/museos/id'>" + pag.nombre + "</li></a>"
+    respuesta += "<html><br>Museos favoritos de gente registrada:<br></html>"
+    lista = Configuracion.objects.all()
+    for pag in lista:
+        respuesta += "<br><li><a href='/usuario'>" + str(pag.usuario) + "</li></a>"
+
+    return HttpResponse(respuesta)
+
+
+def usuario(request):
+    lista = Configuracion.objects.all()
+    for pag in lista:
+        usuario = pag.usuario
+        respuesta = "Los museosfavoritos de " + str(usuario) + " son:<br>"
+        respuesta += str(pag.favoritos) + "<br>"
+        respuesta += "<a href='/'>Volver a la página principal</a><br><a href='/admin'>Ir a la página de configuración</a>"
+
+    return HttpResponse(respuesta)
+
+def museo(request):
+    lista = Comentario.objects.all()
+    for pag in lista:
+        respuesta ="Los comentarios del museo: " + pag.museo + "son los siguientes:<br>"
+        respuesta += pag.comentario + "<br>"
+        respuesta += "<a href='/'>Volver a la página principal</a><br><a href='/admin'>Ir a la página de configuración</a>"
